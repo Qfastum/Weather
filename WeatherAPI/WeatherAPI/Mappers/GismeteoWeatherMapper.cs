5165e0e84095c53a0dcdbc38d3e7c418e059de6c
Qfastum <d5417760@gmail.com>
2025-07-30 21:59:30 +0300

    create Services and Map for GismetioClient

---
+++ WeatherAPI/WeatherAPI/Mappers/GismeteoWeatherMapper.cs
@@ -0,0 +1,27 @@
+﻿using WeatherAPI.Contracts;
+
+namespace WeatherAPI.Mappers
+{
+    public static class GismeteoWeatherMapper
+    {
+
+        public static GismeteoWeatherContract Map(GismetioClient.Contracts.WeatherResponseContract response)
+        {
+            return new GismeteoWeatherContract
+            {
+                LocalDate = response.Date.LocalDate,
+                AirDegreesOfHeat = response.Temperature.Air.DegreesOfHeat,
+                ComfortDegreesOfHeat = response.Temperature.Comfort.DegreesOfHeat,
+                DescriptionFull = response.Description.Full,
+                HumidityPrecent = response.Humidity.Precent,
+                AtmosphericPressure = response.Pressure.AtmosphericPressure,
+                CloudinessPrecent = response.Cloudiness.Precent,
+                CloudinessType = response.Cloudiness.Type,
+                PrecipitationType = response.Precipitation.Type,
+                PrecipitationIntensity =response.Precipitation.Intensity,
+                WindDirectionType = response.Wind.Direction.DirectionType,
+                WindSpeed = response.Wind.Speed.WindSpeed,
+            };
+        }
+    }
+}
---
+++ WeatherAPI/WeatherAPI/Mappers/IdCytiMapper.cs
@@ -0,0 +1,16 @@
+﻿using WeatherAPI.Contracts;
+
+namespace WeatherAPI.Mappers
+{
+    public static class IdCytiMapper
+    {
+        public static IdCytiContract Map(GismetioClient.Contracts.IdCityResponseContract response)
+        {
+            return new IdCytiContract
+            {
+                Id = response.Id,
+            };
+        }
+
+    }
+}
