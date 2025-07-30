1328bcba6f77b1b8f899039363e1fafd1f7b51d4
Qfastum <d5417760@gmail.com>
2025-07-31 01:54:16 +0300

    Create WeatherResponResponseContract.cs

---
+++ WeatherAPI/GismetioClient/Contracts/WeatherResponResponseContract.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GismetioClient.Contracts
+{
+    public class WeatherResponResponseContract
+    {
+        public WeatherResponseContract Response { get; set; }
+    }
+}
